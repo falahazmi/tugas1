c23154170a2c43894721949ac4662058e895712b
falahazmi <azmifa13@gmail.com>
2020-09-29 10:34:32 +0700

    last commit

--- src/__tests__/helpers.js
+++ src/__tests__/helpers.js
@@ -5,11 +5,11 @@
 const {
     incrementQty,
     decrementQty,
-    recalculateSubTtl,
-    absoluteQty
-
+    Subtotal,
+    absoluteQty,
+    Diskon,
+    grandTotal
 } = require('../helpers.js')
-
 
 test('Qty 1 jika ditambah 1 hasilnya 2', () => {
     expect(incrementQty(1)).toBe(2);
@@ -19,10 +19,22 @@ test('Qty 1 jika ditambah "1" hasilnya 2', () => {
     expect(incrementQty("1")).toBe(2);
 });
 
-test('Subtotla dari 25000 dg qty 2 adalah 50000', () => {
-    expect(recalculateSubTtl(25000, 2)).toBe(50000);
+test('Qty 2 jika dikurangi 1 hasilnya 1', () => {
+    expect(decrementQty(2)).toBe(1);
 });
 
-test('nilai absolute dari -1 adalah 1', () => {
-    expect(absoluteQty(-1)).toBe(1);
+test('jika qty -3, maka keluarnya jadi 1', () => {
+    expect(absoluteQty(-3)).toBe(1);
+});
+
+test('Subtotal dari 50000 dn qty 2 adalah 100000', () => {
+    expect(Subtotal(50000, 2)).toBe(100000);
+});
+
+test('Diskon kode 25 dari barang berharga 50k dan qty 2 (total 100k) adalah 25000', () => {
+    expect(Diskon(50000, 2, "25")).toBe(25000);
+});
+
+test('Grandtotal dari barang dengan harga 50000 qty 2 diskon 25 adalah 75000', () => {
+    expect(grandTotal(50000, 2, "25")).toBe(75000);
 });
--- src/helpers.js
+++ src/helpers.js
@@ -1,17 +1,45 @@
 const incrementQty = (qty) => parseInt(qty) + 1;
 const decrementQty = (qty) => parseInt(qty) - 1;
 
-function recalculateSubTtl(price, qty) {
+function Subtotal(price, qty) {
     return price * qty;
 }
 
 function absoluteQty(qty) {
-    return Math.abs(qty);
+    return (parseInt(qty) < 1) ? 1 : qty;
+}
+
+function Diskon(price, qty, kode) {
+    const subTotal = Subtotal(price, qty);
+    switch (kode) {
+        case "AKHIRTAHUN":
+            hasilDiskon = subTotal * 0.90;
+            break;
+        case "50":
+            hasilDiskon = subTotal * 0.50;
+            break;
+        case "25":
+            hasilDiskon = subTotal * 0.25;
+            break;
+        case "10":
+            hasilDiskon = subTotal * 0.10;
+            break;
+        default:
+            hasilDiskon = 0;
+            break;
+    }
+    return hasilDiskon;
+}
+
+function grandTotal(price, qty, kode) {
+    return Subtotal(price, qty) - Diskon(price, qty, kode);
 }
 
 module.exports = {
     incrementQty,
     decrementQty,
-    recalculateSubTtl,
-    absoluteQty
+    Subtotal,
+    absoluteQty,
+    Diskon,
+    grandTotal
 };
